c82ed0ca6f72f90203fdaa2a0f459dfc463a8158
Gururaj TE <gururajte@Gururajs-MacBook-Pro.local>
2025-03-02 07:20:45 +0530

    first

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,6 +2,11 @@ provider "aws" {
     region = "us-east-1"
 }
 
+variable "vpc_name" {
+    name = "firstvpc"
+}
+
 resource "aws_vpc" "test" {
     cidr_block = "10.0.0.0/16"
+    name = var.vpc_name
 }
